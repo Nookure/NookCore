c427d3173e779b8aaa5af7b7e98e2966def72256
Angelillo15 <contact@angelillo15.es>
2023-08-09 01:01:52 +0200

    Added default method loadFile

---
+++ core/src/main/java/es/angelillo15/core/config/Config.kt
@@ -0,0 +1,23 @@
+package es.angelillo15.core.config
+
+import com.google.inject.Inject
+import es.angelillo15.configmanager.ConfigManager
+import es.angelillo15.core.NookInstance
+
+import java.io.File
+
+class Config {
+    @Inject
+    lateinit var plugin: NookInstance
+
+    fun loadFile(original: String, path: String): ConfigManager {
+        ConfigMerge.merge(
+            File("${plugin.getPluginDataFolder().toPath()}/$path"),
+            plugin.getPluginResource(original)
+        )
+
+        val config = ConfigManager(plugin.getPluginDataFolder().toPath(), path, path)
+        config.registerConfig()
+        return config
+    }
+}
