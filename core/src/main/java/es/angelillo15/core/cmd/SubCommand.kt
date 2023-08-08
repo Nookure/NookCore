e34f636add96dcbcea4169c3fbaecc554cb1cfd1
Angelillo15 <contact@angelillo15.es>
2023-08-09 00:48:24 +0200

    Fix subcommand

--- core/src/main/java/es/angelillo15/core/cmd/SubCommand.kt
+++ core/src/main/java/es/angelillo15/core/cmd/SubCommand.kt
@@ -3,9 +3,9 @@ package es.angelillo15.core.cmd
 import es.angelillo15.core.cmd.sender.CommandSender
 
 abstract class SubCommand {
-    abstract val name: String?
-    abstract val description: String?
-    abstract val syntax: String?
-    abstract val permission: String?
+    abstract val name: String
+    abstract val description: String
+    abstract val syntax: String
+    abstract val permission: String
     abstract fun onCommand(sender: CommandSender, label: String, args: Array<String>)
 }
